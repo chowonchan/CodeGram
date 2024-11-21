110595f7946c9e050c9de8e7f77eaee4bc0f618b
LeeNS-2024 <dlsktj1@gmail.com>
2024-11-21 17:44:24 +0900

    스토리 백엔드코드 작성중입니다.

--- src/main/java/edu/kh/cgram/board/mapper/BoardMapper.java
+++ src/main/java/edu/kh/cgram/board/mapper/BoardMapper.java
@@ -5,14 +5,20 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import edu.kh.cgram.board.dto.Board;
+import io.lettuce.core.dynamic.annotation.Param;
 
 @Mapper
 public interface BoardMapper {
 
-	//처음 로드 시 최신 게시물 조회
-	List<Board> findLatestBoards(int page, int pageSize);
+	// 최신 게시물 가져오기
+	List<Board> findLatestBoards(
+			@Param("page") int page, 
+			@Param("pageSize") int pageSize);
 
-	//마지막 게시물 번호 이후로 게시물 조회
-	List<Board> findBoardsAfter(Long lastBoardNo, int page, int pageSize);
+	// 마지막 게시물 번호 이후 게시물 가져오기
+	List<Board> findBoardsAfter(
+			@Param("lastBoardNo") Long lastBoardNo, 
+			@Param("page") int page,
+			@Param("pageSize") int pageSize);
 
 }
