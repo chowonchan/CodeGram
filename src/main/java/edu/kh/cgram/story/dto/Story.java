35c28fe23b81e94ee63adacfa3bd1df240ac8805
LYoungSoo <leeyeong04@gmail.com>
2024-11-25 16:22:23 +0900

    게시글 작성 일부 수정 + 스토리 준비

--- src/main/java/edu/kh/cgram/story/dto/Story.java
+++ src/main/java/edu/kh/cgram/story/dto/Story.java
@@ -20,20 +20,20 @@ public class Story {
 	private int rnum;
 	
 	// STORY 테이블
-	private int 	 storyNo;
+	private int    storyNo;
 	private String imgPath;
 	private String createdAt;
 	private String modifiedAt;
 	private String storyDelFl;
 	private int    readCount;
-	private int		 memberNo;
+	private int	   memberNo;
 	
 	// 스토리 읽음 여부를 저장할 필드 (1 == 읽은 적 있음)
-	private int 	 storyCheck;
+	private int    storyCheck;
 	
 	// 스토리좋아요 체크 여부를 저장할 필드 (1 == 누른 적 있음) 
-	private int		 storyLikeCheck;
+	private int    storyLikeCheck;
 	
-	private String prifileImg;
+	private String profileImg;
 	
 }
