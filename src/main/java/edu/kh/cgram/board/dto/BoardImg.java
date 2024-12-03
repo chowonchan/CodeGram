8885899bac97774e55f1c4c765f5e2b5000dead8
LYoungSoo <leeyeong04@gmail.com>
2024-12-03 10:59:18 +0900

    늦었지만 커밋

--- src/main/java/edu/kh/cgram/board/dto/BoardImg.java
+++ src/main/java/edu/kh/cgram/board/dto/BoardImg.java
@@ -13,9 +13,9 @@ public class BoardImg {
     
     // BOARD_IMG 컬럼과 매핑되는 필드
     private int			imgNo;
-    private String	imgPath;
-    private String	imgRename;
-    private int	    imgOrder;
+    private String	    imgPath;
+    private String	    imgRename;
+    private int	        imgOrder;
     private int			boardNo;
     
     // 게시글 이미지 삽입 / 수정 시 사용할 필드
