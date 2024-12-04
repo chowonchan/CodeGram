c9df10ec5a53fcbe8991ac4f0b3cd01d715f3442
LeeNS-2024 <dlsktj1@gmail.com>
2024-12-04 14:17:59 +0900

    스토리 조회 백엔드 기능구현했습니다.

--- src/main/java/edu/kh/cgram/story/service/StoryService.java
+++ src/main/java/edu/kh/cgram/story/service/StoryService.java
@@ -1,28 +1,20 @@
 package edu.kh.cgram.story.service;
 
 import java.util.List;
+import java.util.Map;
 
 import edu.kh.cgram.story.dto.Story;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface StoryService {
 
-	List<Story> getFollowingStories(String memberNickname);
-
-	Story getStoryDetail(Long storyNo);
-
-	void updateStoryCheck(Long storyNo, String memberNickname);
-	
-	
-	
 	Story storyDetail(String memberNickname, int storyNo);
 	
 	int storyInsert(Story story, MultipartFile image);
+
+	// 메인피드에서 스토리 처리
+	Map<String, Object> updateStoryCheck1(int storyNo, int memberNo);
 	
-	/*
-	 * void insertStoryLike(Long storyNo, String username);
-	 * 
-	 * void deleteStoryLike(Long storyNo, String username);
-	 */
+
 
 }
